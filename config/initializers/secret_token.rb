62cac7b2a7c85b8867379c82e77102e368d72c03
Will Richman <williamrichman@gmail.com>
2013-12-09 22:28:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Curation::Application.config.secret_key_base = '55a46bb0b6f5f9031fde495c79254b67d8beecbc9149b121cd875988bd81db1370ea8ef93c20a1f8152d228b53ce04a517268b62758d8312e72ba5b3912d5e54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Curation::Application.config.session_store :cookie_store, key: '_curation_session'
